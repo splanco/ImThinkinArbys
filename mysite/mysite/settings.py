a5ec3633a3b4b36dc24e5c72bf9bda3f4e7f3102
Spencer Planco <spencerplanco@gmail.com>
2020-05-22 14:26:57 -0400

    Adding User model and making migrations

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
-    'users'.
+    'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
