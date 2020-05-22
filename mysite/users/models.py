b24f4bd1fec7511b295e3a36372155a517857a93
Spencer Planco <spencerplanco@gmail.com>
2020-05-22 14:31:33 -0400

    More migrations and updating name of model

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    First_Name = models.CharField(max_length = 20)
+    Last_Name = models.CharField(max_length = 20)
+    Favorite_Sandwich = models.CharField(max_length = 20)
+    Bio = models.CharField(max_length = 500)
